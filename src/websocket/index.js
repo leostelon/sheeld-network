d2fff705c7756a920151fc4fe52ea11e47b8be73
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2025-05-05 16:50:11 +0530

    fix: minor changes

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -6,16 +6,27 @@ function initializeWebsocket(httpServer) {
 	io.on("connection", (socket) => {
 		console.log("Socket connected:", socket.id);
 
-		// Ping-pong every second
-		setInterval(() => {
-			const start = Date.now();
-			socket.emit("ping");
+		let lastPingTime = null;
 
-			socket.once("pong", () => {
-				const latency = Date.now() - start;
+		// Add the pong listener only once
+		socket.on("pong", () => {
+			if (lastPingTime !== null) {
+				const latency = Date.now() - lastPingTime;
 				socket.emit("latency", latency);
-			});
+			}
+		});
+
+		// Emit ping every second
+		const interval = setInterval(() => {
+			lastPingTime = Date.now();
+			socket.emit("ping");
 		}, 1000);
+
+		// Clean up when socket disconnects
+		socket.on("disconnect", () => {
+			clearInterval(interval);
+			console.log("Socket disconnected:", socket.id);
+		});
 	});
 }
 
