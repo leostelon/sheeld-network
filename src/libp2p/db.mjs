782a1d92a95f8cf797423d7b54b9cf18a2e445c5
leostelon <leostelon17@gmail.com>
2025-05-23 11:26:27 +0530

    chore: update with key and value

--- src/libp2p/db.mjs
+++ src/libp2p/db.mjs
@@ -1,19 +1,44 @@
-import { join } from "path";
 import { Low, JSONFile } from "lowdb";
 import { PORT } from "../constants.js";
+import { getPubSubService } from "./index.mjs";
+import { DB_UPDATE } from "./event_types.mjs";
+import { formatPublishData } from "./util.mjs";
 
+let DB_SYNCED = false;
 // Use JSON file for storage
 const file = `db/lowdb-${PORT}.json`;
 const adapter = new JSONFile(file);
 let dbInitialized = false;
 export const db = new Low(adapter);
 
-export async function writeData() {
-    if (!dbInitialized) return;
-    if (!db.data) {
-        await db.read();
-        db.data = { posts: [] };
-    }
+export async function writeData(key, data) {
+    const pubsub = getPubSubService();
+
+    const keyPath = key.split(".");
+    keyPath.reduce(function (a, b, ind) {
+        if (!a[b]) {
+            a[b] = {};
+        }
+        if (keyPath.length - 1 === ind) {
+            a[b] = data;
+        }
+        return a[b];
+    }, db.data);
+    await db.write();
+    pubsub.publish(DB_UPDATE, formatPublishData({ key, data }));
+}
+
+export async function handleDbUpdate(key, data) {
+    const keyPath = key.split(".");
+    keyPath.reduce(function (a, b, ind) {
+        if (!a[b]) {
+            a[b] = {};
+        }
+        if (keyPath.length - 1 === ind) {
+            a[b] = data;
+        }
+        return a[b];
+    }, db.data);
     await db.write();
 }
 
@@ -24,8 +49,30 @@ export function readData(key) {
 
 export async function initializeDB() {
     await db.read();
-    db.data = { posts: [] };
+    db.data = { nodes: {}, clients: {} };
     await db.write();
     dbInitialized = true;
-    console.log("LOWDB Initianalized");
+}
+
+export async function saveNode(id, node) {
+    const nodes = readData("nodes");
+    nodes[id] = node;
+    await db.write();
+}
+
+export async function updateNodeStatus(peerId, status) {
+    const node = readData(["nodes"][peerId]);
+    node.status = status;
+    await db.write();
+}
+
+export async function syncDB(data) {
+    if (DB_SYNCED) return;
+    db.data = data;
+    DB_SYNCED = true;
+    await db.write();
+}
+
+export async function updateDbSyncStatus(status) {
+    DB_SYNCED = status;
 }
