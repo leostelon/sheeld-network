3b9529fad7e880c29311159a9739ef355a69c0c2
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2025-05-27 10:29:54 +0530

    chore: removed clients.json DB file

--- src/utils/clients.js
+++ src/utils/clients.js
@@ -1,19 +1,10 @@
-const fs = require("fs");
-const path = require("path");
 let { CLIENT_DIR } = require("../constants");
-const CLIENTS_FILE = path.join("db/clients.json");
 
-let writeData;
+let writeData, readData;
 
 // Add or update a key
 function addOrUpdateClientTarget(key, targetNode) {
-    let data = {};
-
-    // Read and parse existing file
-    if (fs.existsSync(CLIENTS_FILE)) {
-        const existingData = fs.readFileSync(CLIENTS_FILE);
-        if (existingData.length !== 0) data = JSON.parse(existingData);
-    }
+    let data = getClients();
 
     // Add or update the key
     data[key] = { ...data[key], ...targetNode };
@@ -24,28 +15,10 @@ function addOrUpdateClientTarget(key, targetNode) {
         };
     }
     writeData(`clients.${key}`, data[key]);
-    updateClientDirectory(data);
-
-    // Save updated data back to file
-    fs.writeFileSync(CLIENTS_FILE, JSON.stringify(data, null, 2));
-}
-
-// Get the value for a key
-function getClientTarget(key) {
-    if (!fs.existsSync(CLIENTS_FILE)) {
-        console.error("Data file not found.");
-        return null;
-    }
-
-    const data = JSON.parse(fs.readFileSync(CLIENTS_FILE, "utf-8"));
-    return data[key] || null;
 }
 
 function getClients() {
-    if (!fs.existsSync(CLIENTS_FILE)) return {};
-    const data = fs.readFileSync(CLIENTS_FILE);
-    if (data.length === 0) return {};
-    return JSON.parse(data);
+    return readData("clients");
 }
 
 function getClientWithSolAddress(solAddress) {
@@ -60,49 +33,37 @@ function getClientWithSolAddress(solAddress) {
 
 async function syncClientsDirectory() {
     console.log("/// SYNCING CLIENTS STARTED ///");
-	const { writeData: wd } = await import("../libp2p/db.mjs");
+	const { writeData: wd, readData: rd } = await import("../libp2p/db.mjs");
 	writeData = wd;
+	readData = rd;
     CLIENT_DIR.clients = getClients();
     console.log("/// SYNCING CLIENTS ENDED ///");
-}
-
-function updateClientDirectory(data) {
-    CLIENT_DIR.clients = data;
 }
 
 function updateClientInboundUsage(clientIp, usage) {
     const client = CLIENT_DIR.clients[clientIp];
     client.usage.received += usage;
-	console.log(client.usage.received);
-    writeData(`clients.${key}.usage.received`, client.usage.received);
-    updateClient(clientIp, "usage", client.usage);
+    writeData(
+		`clients.${clientIp.replaceAll(".", "-")}.usage.received`,
+		client.usage.received
+	);
 }
 
 function updateClientOutboundUsage(clientIp, usage) {
     const client = CLIENT_DIR.clients[clientIp];
     client.usage.sent += usage;
-    writeData(`clients.${key}.usage.sent`, client.usage.sent);
-    updateClient(clientIp, "usage", client.usage);
+    writeData(
+		`clients.${clientIp.replaceAll(".", "-")}.usage.sent`,
+		client.usage.sent
+	);
 }
 
 function updateClientLastPaid(clientIp, last_paid) {
-    updateClient(clientIp, "last_paid", last_paid);
-}
-
-function updateClient(clientIp, key, value) {
-    CLIENT_DIR.clients[clientIp][key] = value;
-    fs.writeFile(
-        CLIENTS_FILE,
-        JSON.stringify(CLIENT_DIR.clients, null, 2),
-        (err) => {
-            if (err) throw err;
-        }
-    );
+	throw new Error("Client payment not implemented!")
 }
 
 module.exports = {
     addOrUpdateClientTarget,
-    getClientTarget,
     syncClientsDirectory,
     updateClientInboundUsage,
     updateClientOutboundUsage,
